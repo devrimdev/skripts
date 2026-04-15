6c462c7db01d66172638dcb3e2d5232b6ff894bf
devrimdev <devrimdevelop@gmail.com>
2026-04-15 09:16:55 +0000

    U

---
+++ scripts/pki/scp.sh
@@ -0,0 +1,8 @@
+scp ~/certs/ca.crt \
+    ~/certs/monitoring.crt ~/certs/monitoring.key \
+    ~/certs/pcap.crt ~/certs/pcap.key \
+    ~/certs/analysis.crt ~/certs/analysis.key \
+    ~/certs/mintclient.crt ~/certs/mintclient.key \
+    ~/certs/pfsense.crt ~/certs/pfsense.key \
+    ~/certs/prometheus-client.crt ~/certs/prometheus-client.key \
+    student@bastion.gfn.internal:~/certs/
